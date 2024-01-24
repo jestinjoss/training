776265a121c1665a126dfd7ac6d16437686d0db1
jayrkumar <jay@heartsource.co>
2024-01-24 11:31:28 -0500

    Assignment for the team

--- tests/read_file.py
+++ tests/read_file.py
@@ -1,6 +1,33 @@
-file = open("../data/test.txt","r")
+def count_lines(content):
+    print("No of lines: #")
+
+def count_words(content):
+    print("No of words: #")
+    
+def print_alternate_lines(content):
+    print("Alternate lines go here")
+
+
+file = open("./data/test.txt","r")
 content=file.read()
-print(content)
+print("PRINTING NUMBER OF LINES")
+print("========================")
+count_lines(content)
+print("========================")
+
+print("PRINTING NUMBER OF WORDS")
+print("========================")
+count_words(content)
+print("========================")
+
+print("PRINTING ALTERNATE LINES")
+print("========================")
+print_alternate_lines(content)
+print("========================")
+
 file.close()
 # adding comment
 # adding comment in the branc code
+
+    
+    
